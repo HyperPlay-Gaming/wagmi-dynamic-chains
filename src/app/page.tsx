3c0b49ae790d7b53c6861577acf10b2aefa594da
Elio Briceño <eliobricenov@gmail.com>
2024-05-08 11:26:02 -0400

    tech: initial POC

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,74 @@
-'use client'
+"use client";
 
-import { useAccount, useConnect, useDisconnect } from 'wagmi'
+import { useContext, useEffect } from "react";
+import { Chain } from "viem/chains";
+import {
+  createConfig,
+  http,
+  useAccount,
+  useConnect,
+  useDisconnect,
+  useSwitchChain,
+} from "wagmi";
+import { WagmiChainsContext } from "./providers";
+import { injected, coinbaseWallet, walletConnect } from "wagmi/connectors";
+
+function useWagmiChains() {
+  const { chains, setChains, nonConfiguredChains, ...rest } =
+    useContext(WagmiChainsContext);
+
+  const addChain = (chain: Chain) => {
+    setChains((prev) => [...prev, chain]);
+  };
+
+  return { chains, addChain, nonConfiguredChains, ...rest };
+}
 
 function App() {
-  const account = useAccount()
-  const { connectors, connect, status, error } = useConnect()
-  const { disconnect } = useDisconnect()
+  const account = useAccount();
+  const { connectors, connect, status, error } = useConnect();
+  const { disconnect } = useDisconnect();
+  const { chains, switchChain } = useSwitchChain();
+  const {
+    addChain,
+    nonConfiguredChains,
+    setNonConfiguredChains,
+    setConfig,
+    chains: contextChain,
+  } = useWagmiChains();
+
+  useEffect(() => {
+    if (!process.env.NEXT_PUBLIC_WC_PROJECT_ID) {
+      throw new Error("NEXT_PUBLIC_WC_PROJECT_ID is required");
+    }
+
+    const newTransports = contextChain.reduce(
+      (acc, chain) => {
+        acc[chain.id] = http();
+        return acc;
+      },
+      {} as Record<string, ReturnType<typeof http>>
+    );
+
+    const newConfig = createConfig({
+      // @ts-expect-error
+      chains: contextChain,
+      connectors: [
+        injected(),
+        coinbaseWallet({ appName: "Create Wagmi" }),
+        walletConnect({ projectId: process.env.NEXT_PUBLIC_WC_PROJECT_ID }),
+      ],
+      ssr: true,
+      transports: newTransports,
+    });
+
+    setConfig(newConfig);
+    setNonConfiguredChains(
+      nonConfiguredChains.filter(
+        (chain) => !contextChain.some((c) => c.id === chain.id)
+      )
+    );
+  }, [contextChain]);
 
   return (
     <>
@@ -20,7 +83,7 @@ function App() {
           chainId: {account.chainId}
         </div>
 
-        {account.status === 'connected' && (
+        {account.status === "connected" && (
           <button type="button" onClick={() => disconnect()}>
             Disconnect
           </button>
@@ -41,8 +104,29 @@ function App() {
         <div>{status}</div>
         <div>{error?.message}</div>
       </div>
+
+      <div>
+        <h2>Avilable Chains</h2>
+        {chains.map((chain) => (
+          <button
+            key={chain.id}
+            onClick={() => switchChain({ chainId: chain.id })}
+          >
+            {chain.name}
+          </button>
+        ))}
+      </div>
+
+      <div>
+        <h2>Non-configured Chains</h2>
+        {nonConfiguredChains.map((chain) => (
+          <button key={chain.id} onClick={() => addChain(chain)}>
+            {chain.name}
+          </button>
+        ))}
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -1,19 +1,79 @@
-'use client'
+"use client";
 
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
-import { useState, type ReactNode } from 'react'
-import { WagmiProvider } from 'wagmi'
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { createContext, useEffect, useState, type ReactNode } from "react";
+import { Config, WagmiProvider, createConfig, http } from "wagmi";
+import { coinbaseWallet, injected, walletConnect } from "wagmi/connectors";
 
-import { config } from '@/wagmi'
+import { config as defaultConfig } from "@/wagmi";
+import {
+  polygon,
+  acala,
+  ancient8,
+  ancient8Sepolia,
+  saigon,
+  scroll,
+  canto,
+  celo,
+  cronos,
+  Chain,
+} from "viem/chains";
+
+const defaultNonConfiguredChains = [
+  polygon,
+  acala,
+  ancient8,
+  ancient8Sepolia,
+  saigon,
+  scroll,
+  canto,
+  celo,
+  cronos,
+];
+
+export const WagmiChainsContext = createContext<{
+  config: Config;
+  chains: Chain[];
+  nonConfiguredChains: Chain[];
+  setChains: React.Dispatch<React.SetStateAction<Chain[]>>;
+  setNonConfiguredChains: React.Dispatch<React.SetStateAction<Chain[]>>;
+  setConfig: React.Dispatch<React.SetStateAction<Config>>;
+}>({
+  config: defaultConfig,
+  chains: [],
+  nonConfiguredChains: [],
+  setChains: () => {},
+  setNonConfiguredChains: () => {},
+  setConfig: () => {},
+});
 
 export function Providers(props: { children: ReactNode }) {
-  const [queryClient] = useState(() => new QueryClient())
+  const [queryClient] = useState(() => new QueryClient());
+  const [config, setConfig] = useState(defaultConfig);
+
+  // @ts-expect-error
+  const [chains, setChains] = useState(defaultConfig.chains as Chain[]);
+  const [nonConfiguredChains, setNonConfiguredChains] = useState(
+    defaultNonConfiguredChains as Chain[]
+  );
 
   return (
-    <WagmiProvider config={config}>
-      <QueryClientProvider client={queryClient}>
-        {props.children}
-      </QueryClientProvider>
-    </WagmiProvider>
-  )
+    <WagmiChainsContext.Provider
+      value={{
+        config,
+        nonConfiguredChains,
+        chains,
+        setChains,
+        setNonConfiguredChains,
+        // @ts-expect-error
+        setConfig,
+      }}
+    >
+      <WagmiProvider config={config}>
+        <QueryClientProvider client={queryClient}>
+          {props.children}
+        </QueryClientProvider>
+      </WagmiProvider>
+    </WagmiChainsContext.Provider>
+  );
 }
